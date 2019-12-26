56c65e56bf6ebe9bab6d579a55a28ecfa41409a0
Davinder Singh Kharoud <dskharoud2@gmail.com>
2019-12-26 10:06:01 -0500

    Configuration Scan.

--- diexample/src/main/java/kharoud/springframework/di/springBoot/DiApplication.java
+++ diexample/src/main/java/kharoud/springframework/di/springBoot/DiApplication.java
@@ -1,12 +1,13 @@
-package kharoud.springframework.di;
+package kharoud.springframework.di.springBoot;
 
 import kharoud.springframework.di.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import sun.java2d.windows.GDIRenderer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("kharoud.springframework.di")
 public class DiApplication {
 
     public static void main(String[] args) {
