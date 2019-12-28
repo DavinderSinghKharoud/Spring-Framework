625813e0917337e7bc7b73e3078fc248ac8ad44d
Davinder Singh Kharoud <dskharoud2@gmail.com>
2019-12-27 23:28:00 -0500

    XML configuration.

--- diexample/src/main/java/kharoud/springframework/di/controllers/GreetingController.java
+++ diexample/src/main/java/kharoud/springframework/di/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package kharoud.springframework.di.controllers;
 
 import kharoud.springframework.di.services.HelloWorldService;
+import kharoud.springframework.di.services.HelloWorldServiceHindhiImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,15 +11,35 @@ public class GreetingController {
 
     private HelloWorldService helloWorldService;
 
+    private HelloWorldService helloWorldServiceHindhi;
+
+    private HelloWorldService helloWorldServiceFrench;
+
     @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
+    @Autowired
+    @Qualifier("helloWorldServiceHindhi")
+    public void setHelloWorldServiceHindhi(HelloWorldService helloWorldServiceHindhi){
+        this.helloWorldServiceHindhi = helloWorldServiceHindhi;
+    }
+
+    @Autowired
+    @Qualifier("french")
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench){
+        this.helloWorldServiceFrench = helloWorldServiceFrench;
+    }           
+
     public String sayHello(){
 
         String greeting = helloWorldService.getGreeting();
         System.out.println(greeting);
+
+        System.out.println(helloWorldServiceHindhi.getGreeting());
+
+        System.out.println(helloWorldServiceFrench.getGreeting());
         return greeting;
     }
 }
